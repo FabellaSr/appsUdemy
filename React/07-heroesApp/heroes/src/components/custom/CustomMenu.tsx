4edb7de8283df237ae97dd8810632ba425d009a0
FabellaSr <fabellaim@outlook.com>
2026-03-29 23:21:17 -0300

    varios cambios
    s

---
+++ React/07-heroesApp/heroes/src/components/custom/CustomMenu.tsx
@@ -0,0 +1,46 @@
+import { Link, useLocation } from "react-router"
+
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "../ui/navigation-menu"
+import { cn } from "@/lib/utils";
+
+
+
+export const CustomMenu = () => {
+    const { pathname } = useLocation();
+
+    const isActive = (path: string) => pathname === path;
+
+
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+
+                <NavigationMenuItem>
+                    <NavigationMenuLink
+                        asChild
+                        className={cn(isActive('/') && 'bg-slate-200 rounded-mdp-2')}>
+                        <Link to="/">Home</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+
+                <NavigationMenuItem>
+                    <NavigationMenuLink 
+                        asChild 
+                        className={cn(isActive('/Search') && 'bg-slate-200 rounded-mdp-2')}>
+                        <Link to="/Search">Search superHeroes</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+
+                <NavigationMenuItem>
+                    <NavigationMenuLink 
+                        asChild 
+                        className={cn(isActive('/About') && 'bg-slate-200 rounded-mdp-2')}>
+                        <Link to="/About">About</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+
+
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
--- React/07-heroesApp/heroes/src/heroes/pages/search/SearchPage.tsx
+++ React/07-heroesApp/heroes/src/heroes/pages/search/SearchPage.tsx
@@ -1,11 +1,17 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControl } from "./ui/SearchControl";
+ 
 
 export const SearchPage = () => {
   return (
     <>
       <CustomJumbotron title={"Search Fermin"} description="Fermin Fabella Medina" />
+      {/* Stats Dashboard */}
       <HeroStats />
+      {/* Search and Filters */}
+      <SearchControl />
+      
     </>
   )
 }
