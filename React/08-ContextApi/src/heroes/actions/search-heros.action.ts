5398448f93df8c39d1db1892a3542733d6671025
FabellaSr <fabellaim@outlook.com>
2026-04-11 22:33:52 -0300

    mas

--- React/08-ContextApi/src/heroes/actions/search-heros.action.ts
+++ React/08-ContextApi/src/heroes/actions/search-heros.action.ts
@@ -1,9 +1,37 @@
-import { heroApi } from "../api/hero.api"
-import type { searchHeroes } from "../types/search-heros.response"
+import { heroApi } from '../api/hero.api';
+import type { Hero } from '../types/hero.interface';
 
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
-
-export const searchHeroesActions = async () => {
-    const { data } = await heroApi.get<searchHeroes>(`/search`);
-    return data;
+interface Options {
+  name?: string;
+  team?: string;
+  category?: string;
+  universe?: string;
+  status?: string;
+  strength?: string;
 }
+
+export const searchHeroesAction = async (options: Options = {}) => {
+  const { name, team, category, universe, status, strength } = options;
+
+  if (!name && !team && !category && !universe && !status && !strength) {
+    return [];
+  }
+
+  const { data } = await heroApi.get<Hero[]>('/search', {
+    params: {
+      name,
+      team,
+      category,
+      universe,
+      status,
+      strength,
+    },
+  });
+
+  return data.map((hero) => ({
+    ...hero,
+    image: `${VITE_API_URL}/images/${hero.image}`,
+  }));
+};
--- React/08-ContextApi/src/heroes/pages/search/SearchPage.tsx
+++ React/08-ContextApi/src/heroes/pages/search/SearchPage.tsx
@@ -1,10 +1,26 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { useSearchParams } from "react-router";
+
+import { HeroGrid } from "@/heroes/components/HeroGrid";
 import { HeroStats } from "@/heroes/components/HeroStats";
-import { SearchControl } from "./ui/SearchControl";
+import { searchHeroesAction } from "@/heroes/actions/search-heros.action";
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { SearchControl } from "./ui/SearchControl";
+import { useQuery } from '@tanstack/react-query';
  
 
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get('name') ?? undefined;
+  const strength = searchParams.get('strength') ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ['search', { name, strength }],
+    queryFn: () => searchHeroesAction({ name, strength }),
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+
   return (
     <>
       <CustomJumbotron title={"Search Fermin"} description="Fermin Fabella Medina" />
@@ -13,8 +29,10 @@ export const SearchPage = () => {
       <HeroStats />
       {/* Search and Filters */}
       <SearchControl />
-      
+      <HeroGrid heroes={heroes} />
     </>
   )
 }
+
 export default SearchPage;
+
